aac5e122fc954e4bb875306ac8f9993604d525b8
Damian <emer.dzc@gmail.com>
2020-06-08 09:53:37 -0500

    Activando cierre de sesión

--- src/app/features/process/components/title-header/title-header.component.ts
+++ src/app/features/process/components/title-header/title-header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/shared/models/store.state.interface";
+import { signOut } from "src/app/features/landing/store/login/login.action";
 
 @Component({
   selector: "app-title-header",
@@ -9,7 +12,11 @@ export class TitleHeaderComponent implements OnInit {
   @Input() titlePath: string;
   @Input() path: string;
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {}
+
+  signOut() {
+    this.store.dispatch(signOut());
+  }
 }
